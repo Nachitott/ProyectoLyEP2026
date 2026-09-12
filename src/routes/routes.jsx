313841979d5304a4a1ea95138ac8f6e21a166a6b
MiguelAbregu <miguel_af_555@hotmail.com>
2026-09-12 03:39:55 -0300

    refactor(routes): reestructurar routes.jsx con rutas anidadas protegidas y limpiar Navigate no usado [MiguelAbregu]

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import Login from '../pages/Login'
 import Dashboard from '../pages/Dashboard'
@@ -6,38 +6,31 @@ import ListaClientes from '../pages/ListaClientes'
 import DetalleCliente from '../pages/DetalleCliente'
 import ErrorPage from '../pages/ErrorPage'
 import RutaProtegida from '../components/RutaProtegida'
+import MainLayout from '../layouts/MainLayout'
+
 const AppRoutes = () => {
   return (
     <Routes>
-
+      {/* Ruta pública sin Header ni Nav */}
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/"
-        element={
-          <RutaProtegida>
-            <Dashboard />
-          </RutaProtegida>
-        }
-      />
-      <Route
-        path="/clientes"
-        element={
-          <RutaProtegida>
-            <ListaClientes />
-          </RutaProtegida>
-        }
-      />
-      <Route
-        path="/clientes/:id"
-        element={
-         <RutaProtegida>
-          <DetalleCliente />
-         </RutaProtegida>
-      }
-      />
-      <Route path="*" element={<ErrorPage />} />
 
+      {/* Rutas protegidas envueltas en MainLayout */}
+      <Route
+        element={
+          <RutaProtegida>
+            <MainLayout />
+          </RutaProtegida>
+        }
+      >
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/clientes" element={<ListaClientes />} />
+        <Route path="/clientes/:id" element={<DetalleCliente />} />
+      </Route>
+
+      {/* Ruta 404 */}
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
   )
 }
+
 export default AppRoutes
